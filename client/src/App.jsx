58eb6405264bfecdff87b680c11d091b33c8c3c9
fborjaz <fborjaz@unemi.edu.ec>
2023-10-26 16:37:00 -0500

    frontend

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,14 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import { TasksPage } from './pages/TasksPage'
 import { TasksFormPage } from './pages/TasksFormPage'
+import Home from './components/Home'
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path = "/" element = {<Navigate to = "/shopping" />} />
-        <Route path = "/shopping" element = {<TasksPage />} />
+        <Route path = "/shopping" element = {<Home />} />
         <Route path = "/shopping-create" element = {<TasksFormPage />} />
       </Routes>
     </BrowserRouter>
