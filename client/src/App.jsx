6320203300e11b0c1dc771966f32dd2dd960280a
fborjaz <fborjaz@unemi.edu.ec>
2023-10-26 18:36:21 -0500

    examplo frontend

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,12 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
-import Home from './components/Home'
+import Main from './components/Main'
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path = "/" element = {<Navigate to = "/shopping" />} />
-        <Route path = "/shopping" element = {<Home />} />
+        <Route path = "/shopping" element = {<Main />} />
       </Routes>
     </BrowserRouter>
   )
